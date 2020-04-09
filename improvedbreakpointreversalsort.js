ee9f741749830e9c30fd683d1bd450c121624e5b
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-04-09 10:52:14 +0200

    Final Commit

--- improvedbreakpointreversalsort.js
+++ improvedbreakpointreversalsort.js
@@ -3,15 +3,20 @@ var fs = require('fs');
 module.exports = {
     main: function (array, res) {
         var permutation = array.map(Number); //Input file
+        permutation.unshift(0)
+        permutation.push(permutation.length)
+        console.log(permutation);
+
         // var permutation = [0, 13, 2, 17, 1, 3, 20, 19, 11, 12, 4, 5, 16, 15, 10, 18, 14, 8, 7, 6, 9, 21]
         var smallest = permutation.length //smallest var starts with value of maximum number
-        var reversed, increasing = [] 
+        var reversed, increasing = []
         var breakNum //Number of breakpoints
+        var p = 0 //Number of breakpoints
 
         findBreakPoint() //Function that gives number of breakpoints and smallest number in decreasing sequence to enter in while loop
         while (breakNum > 0) { //While number of breakpoints is higher than zero
             findBreakPoint() //Call function each cycle for number of breakpoints
-            if (smallest != permutation.length) {  //if smallest number is still equal to maximum number, it mens that there was no decreasing sequence
+            if (smallest != permutation.length) { //if smallest number is still equal to maximum number, it mens that there was no decreasing sequence
                 i = permutation.indexOf(smallest) //We take the index of smallest number in a decreasing sequence
                 j = permutation.indexOf(smallest - 1) //We take the index of the number before that (smallest - 1)
 
@@ -23,7 +28,12 @@ module.exports = {
                     reversal(j, i)
                 }
             } else if (breakNum > 0) { //In the case that there's no decreasing sequence
-                findIncreasing() //Gets an increasing sequence 
+                findIncreasing(0) //Gets an increasing sequence
+                if (increasing.includes(0)) {
+                    num = increasing[increasing.length - 1]
+                    increasing = []
+                    findIncreasing(num)
+                }
                 first = permutation.indexOf(increasing[0])
                 last = permutation.indexOf(increasing[increasing.length - 1])
                 reverseIncreased(first, last) //revert the sequence
@@ -31,8 +41,15 @@ module.exports = {
             }
         }
 
-        function findIncreasing() {
-            for (let i = 0; i < permutation.length - 1; i++) {
+        fs.appendFileSync("./filesWrite.txt",
+            "----------------------------- Simple Reversal Sort ----------------------------- \n" +
+            "Number of reversals: " + p + "\n" +
+            "Identity permutation: " + permutation + "\n"
+        );
+        res.download(`${__dirname}/filesWrite.txt`); // Set disposition and send it.
+
+        function findIncreasing(init) {
+            for (let i = init; i < permutation.length - 1; i++) {
                 if (permutation[i] - permutation[i + 1] == -1) { //If it's an increasing sequence
                     increasing.push(permutation[i], permutation[i + 1]) //Saves to an array the numbers of the sequence
                 } else if (increasing.length != 0) { //If the array is filled exit loop
@@ -60,6 +77,7 @@ module.exports = {
             permutation = [].concat(...permutation) //Flattens the array
             reversed = []
             console.log(permutation);
+            p = p + 1
         }
 
         function reverseIncreased(left, right) { //Especial case for Reverting Increasing sequences
@@ -68,8 +86,9 @@ module.exports = {
             reversed.reverse()
             permutation.splice(left, 0, reversed)
             permutation = [].concat(...permutation)
-            reversed = []
+            reversed
             console.log(permutation);
+            p = p + 1
         }
     }
 }
